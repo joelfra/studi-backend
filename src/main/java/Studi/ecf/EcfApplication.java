dbda1887af49dad3ca19e1001b61a0fa2f2d5a21
joel <gaucherjoel@hotmail.fr>
2024-07-10 08:18:05 +0200

    first commit

---
+++ src/main/java/Studi/ecf/EcfApplication.java
@@ -0,0 +1,13 @@
+package Studi.ecf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcfApplication.class, args);
+	}
+
+}
