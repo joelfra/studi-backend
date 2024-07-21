239b55349f9faf79f1dd1157f9ade96a0a112ceb
joel <gaucherjoel@hotmail.fr>
2024-07-21 14:41:26 +0200

    fix files codedeploy

--- scripts/fix_previleges.sh
+++ scripts/fix_previleges.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-chmod +x /home/ec2-user/ecf-0.0.1-SNAPSHOT.jar
-chmod +x /home/ec2-user/start_server.sh
-chmod +x /home/ec2-user/stop_server.sh
+chmod +x /home/ubuntu/ecf-0.0.1-SNAPSHOT.jar
+chmod +x /home/ubuntu/start_server.sh
+chmod +x /home/ubuntu/stop_server.sh
